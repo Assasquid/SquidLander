fd3948a0eb332d81553cbd78170ccc1c4f762077
AssaSquid <jerome@nebula-tales.com>
2020-01-27 07:38:28 +0000

    Added colliders to the player and a rigidbody. Added inputs recognition and and a thrust.

---
+++ Assets/Squid.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Squid : MonoBehaviour
+{
+    Rigidbody rigidBody;
+
+    private void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidBody.AddRelativeForce(Vector3.up);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            print("A Pressed.");
+        }
+
+        else if (Input.GetKey(KeyCode.D))
+        {
+            print("D Pressed.");
+        }
+    }
+}
