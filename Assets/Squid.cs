5f86939948dd7d4dac3ac1daa19358d31726cfbb
AssaSquid <jerome@nebula-tales.com>
2020-01-28 08:04:36 +0000

    Added sound, changed the graivity settings.

--- Assets/Squid.cs
+++ Assets/Squid.cs
@@ -6,24 +6,40 @@ using UnityEngine;
 public class Squid : MonoBehaviour
 {
     Rigidbody rigidBody;
+    AudioSource squidSwim;
 
     private void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
+        squidSwim = GetComponent<AudioSource>();
     }
 
     void Update()
     {
-        ProcessInput();
+        Swim();
+        Rotate();
     }
 
-    private void ProcessInput()
+    private void Swim()
     {
         if (Input.GetKey(KeyCode.Space))
         {
             rigidBody.AddRelativeForce(Vector3.up);
+            if (!squidSwim.isPlaying) // so it doesn't layer
+            {
+                squidSwim.Play();
+            }
         }
+        // the following is not necessary in our case, the sound is a one shot
+        // maybe consider using an array of random sounds to make it sound better
+        /*else 
+        {
+            squidSwim.Stop();
+        }*/
+    }
 
+    private void Rotate()
+    {
         if (Input.GetKey(KeyCode.A))
         {
             transform.Rotate(Vector3.forward);
