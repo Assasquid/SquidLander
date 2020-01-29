b83647440092ce34a039ea57116d5b733f3a57be
AssaSquid <jerome@nebula-tales.com>
2020-01-29 08:14:08 +0000

    Added a way to change the rotation speed but also the movement speed.

--- Assets/Squid.cs
+++ Assets/Squid.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 
 public class Squid : MonoBehaviour
 {
+    [SerializeField] float speedSwim = 200f;
+    [SerializeField] float rcsSwim = 300f;
+
     Rigidbody rigidBody;
     AudioSource squidSwim;
 
@@ -18,13 +21,15 @@ public class Squid : MonoBehaviour
     {
         Swim();
         Rotate();
+
     }
 
     private void Swim()
     {
         if (Input.GetKey(KeyCode.Space))
         {
-            rigidBody.AddRelativeForce(Vector3.up);
+            float speedThisFrame = speedSwim * Time.deltaTime;
+            rigidBody.AddRelativeForce(Vector3.up * speedThisFrame);
             if (!squidSwim.isPlaying) // so it doesn't layer
             {
                 squidSwim.Play();
@@ -41,14 +46,17 @@ public class Squid : MonoBehaviour
     private void Rotate()
     {
         rigidBody.freezeRotation = true; // take manual control of rotation
+
+        float rotationThisFrame = rcsSwim * Time.deltaTime;
+
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(Vector3.forward);
+            transform.Rotate(Vector3.forward * rotationThisFrame);
         }
 
         else if (Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(-Vector3.forward);
+            transform.Rotate(-Vector3.forward * rotationThisFrame);
         }
 
         rigidBody.freezeRotation = false; // resume physics control of rotation
